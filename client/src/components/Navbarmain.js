44cfc398126c672fb196718dd0aedb585cf21998
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2022-08-21 13:33:05 +0530

    commit 01

--- client/src/components/Navbarmain.js
+++ client/src/components/Navbarmain.js
@@ -74,7 +74,7 @@ function Navbarmain() {
         function handleResize() {
             console.log(windowWidth);
 
-            if (window.innerWidth < 550) {
+            if (window.innerWidth < 650) {
                 console.log("Came");
 
                 document.getElementById("vdcLogo").classList.remove("ms-auto");
