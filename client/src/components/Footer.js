e44a933b29e38a8e2bf2bd1c024ae75fcdde8ec8
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2022-08-21 20:08:14 +0530

    update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div className="footer" style={{padding:"2% 0"}}>
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-4 home">
+                    <div className="col-12 col-sm-4 home footerBlock">
                         <h3 className="header" style={{color:'whitesmoke'}}>Links</h3>
                         <ul style={{listStyle:"none", textAlign:"left", padding:0}}>
                             <li><a>Home</a></li>
@@ -14,7 +14,7 @@ function Footer() {
                             <li><a>Contact Us</a></li>
                         </ul>
                     </div>  
-                    <div className="col-12 col-sm-4 home">
+                    <div className="col-12 col-sm-4 home footerBlock">
                         <h3 className="header" style={{color:"whitesmoke"}}>Our Address</h3>
                         <ul style={{listStyle:"none", textAlign:"left", padding:0}}>
                             <li>GITAM Institute of Technology</li>
@@ -23,7 +23,7 @@ function Footer() {
                             <li>+12 34567890</li>
                         </ul>
                     </div>
-                    <div className="col-12 col-sm-4 align-self-center">
+                    <div className="col-12 col-sm-4 align-self-center footerBlock">
                         <div className="text-center">
                             <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
                             <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
@@ -35,7 +35,7 @@ function Footer() {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-12" style={{textAlign:"center"}}>
+                    <div className="col-12 footerBlock" style={{textAlign:"center"}}>
                         <h4 className="home">Made with ♥ by Team VDC</h4>
                     </div>
                 </div>
