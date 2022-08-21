42885feeda473fc2ecb7a91a345f4e5c45b9f499
Ajaykumar-13 <ajaykumar13.p@gmail.com>
2022-08-21 14:36:01 +0530

    update 04

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -11,7 +11,7 @@ function Jumbotron(props) {
                     <div className="col-12 col-lg-6">
                         <h1 className="header">E-CLUB</h1>
                         <h3 className="header">Unleash your Entreprenerial Spirit</h3>
-                        <p className="home">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <p className="home">{window.innerWidth}+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                     <div className="col-12 col-lg-3 d-none d-sm-none d-md-block" style={{textAlign: 'center'}}>
                         <img
